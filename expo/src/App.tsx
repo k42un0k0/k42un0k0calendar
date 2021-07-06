da763c32dab2368ff09d6d0b50104f938d22edbb
かずのこ <k42un0k0.put1put1@gmail.com>
2021-07-06 20:48:21 +0900

    aa

--- expo/src/App.tsx
+++ expo/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { RecoilRoot } from 'recoil';
 import Route from './screens';
+import firebase from "firebase/app"
+import {firebaseConfig} from "../firebase.config"
+import { useEffect } from 'react';
 
 export default function App() {
+  useEffect(()=>{
+    firebase.initializeApp(firebaseConfig);
+  },[])
   return (
     <RecoilRoot>
       <Route />
